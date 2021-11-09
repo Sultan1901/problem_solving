7ab537e3ebd5ef5efd2a1b16c2236c146439ac71
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-09 16:40:52 +0300

    update w05d03

--- main.js
+++ main.js
@@ -14,3 +14,20 @@ let fizzBuzz = (n) => {
   };
   
   fizzBuzz(9);
+
+
+  // problem solving W05D03
+
+  const whosOnline = (friends) => {
+    return friends.reduce((acc, friend) => {
+      if (friend.status !== 'offline' && friend.lastActivity > 10) {
+        acc['away'] = acc['away'] || [];
+        acc['away'].push(friend.username);
+      }
+      else {
+        acc[friend.status] = acc[friend.status] || [];
+        acc[friend.status].push(friend.username);
+      }
+      return acc;
+    }, {});
+  };
